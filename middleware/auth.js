215814df73c12fef52cef16311830f1dbd68b936
gktnwls1127 <gktnwls1127@naver.com>
2020-07-01 09:38:23 +0900

    en

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
-const User = require("../models/User");
+const { User } = require("../models/User");
 
-const auth = (req, res, nex) => {
+const auth = (req, res, next) => {
     //인증 처리를 하는 곳
 
     //클라이언트 쿠키에서 토큰을 가져온다
--- models/User.js
+++ models/User.js
@@ -91,11 +91,11 @@ userSchema.statics.findByToken = function(token, cb) {
 
     //user._id + '' = token
     //토큰을 decode한다
-    jwt.verify(token, 'secretToken', function(err, decoded){
+    jwt.verify(token, 'secretToken', function(err, decode){
         //유저 아이디를 이용해서 유저를 찾은 다음에
         // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
 
-        user.findOne({"_id" : decoded, "token" : token}, function(err, user) {
+        user.findOne({"_id" : decode, "token" : token}, function(err, user) {
             if(err) return cb(err)
             cb(null, user)
         })
